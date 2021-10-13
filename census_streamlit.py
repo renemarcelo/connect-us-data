27d40af99c4f073a2d0895c9e9fdadd853aff884
John Bruscella <65571523+jabrusce@users.noreply.github.com>
2021-10-13 11:50:40 -0400

    remove import kmeans

--- census_streamlit.py
+++ census_streamlit.py
@@ -4,7 +4,7 @@ import numpy as np
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 import pandas as pd
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
