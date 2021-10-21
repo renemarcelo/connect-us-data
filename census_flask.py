9b98ec25c91654ed874ef880a8ad7c527bfcc513
John Bruscella <65571523+jabrusce@users.noreply.github.com>
2021-10-21 17:49:40 -0400

    Update census_flask.py

--- census_flask.py
+++ census_flask.py
@@ -17,30 +17,44 @@ def county():
     cols = data.columns[1:]
     print('line18')
 
-#     scaler = StandardScaler()
-#     X_scaled = scaler.fit_transform(data[cols])
+    scaler = StandardScaler()
+    print('line21')
+    X_scaled = scaler.fit_transform(data[cols])
+    print('line23')
     
-    # kmeans = KMeans(n_clusters=200)
-    # kmeans.fit(X_scaled)
+    kmeans = KMeans(n_clusters=200)
+    print('line26')
+    kmeans.fit(X_scaled)
+    print('line28')
 
-    # data['label'] = kmeans.labels_
-    # y = data.label
+    data['label'] = kmeans.labels_
+    print('line31')
+    y = data.label
+    print('line33')
 
-#     knn_model = pickle.load(open('./model/knn_updated.pkl', 'rb'))
-# #     knn_model.fit(X_scaled, y)
+    knn_model = pickle.load(open('./model/knn_updated.pkl', 'rb'))
+    print('line36')
+#     knn_model.fit(X_scaled, y)
     
-#     pref={'median_income': [500_000], 
-#         'temp': [70], 
-#         'total_population': [80_000],
-#         'elev_in_ft': [80]}
+    pref={'median_income': [500_000], 
+        'temp': [70], 
+        'total_population': [80_000],
+        'elev_in_ft': [80]}
+    print('line43')
     
-#     prefdf = pd.DataFrame.from_dict(pref, orient='index').T
-#     pref_scaled = scaler.transform(prefdf)
-#     pred = knn_model.predict(pref_scaled)[0]
+    prefdf = pd.DataFrame.from_dict(pref, orient='index').T
+    print('line46')
+    pref_scaled = scaler.transform(prefdf)
+    print('line48')
+    pred = knn_model.predict(pref_scaled)[0]
+    print('line50')
     
-#     user_df = data[data.label == pred].county
-#     user_show = pd.DataFrame(user_df)
-#     return(user_show.to_dict())
+    user_df = data[data.label == pred].county
+    print('line53')
+    user_show = pd.DataFrame(user_df)
+    print('line55')
+    return(user_show.to_dict())
+    print('line57')
     return "<h1>Welcome to test</h1>"
 
 if __name__ == "__main__":
