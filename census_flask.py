fa053c57b58bce806b7ec8eb07ead3ffe049709d
cmosley <cmosley@beholdmedia.com>
2021-11-02 21:33:11 -0500

    POST request for API call

--- census_flask.py
+++ census_flask.py
@@ -1,57 +1,64 @@
-from flask import Flask, Response, request, jsonify, render_template
+from flask import Flask, Response, json, request, jsonify, render_template
 import numpy as np
 import pandas as pd
 import pickle
+# from flask_cors import CORS
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-import time
 
 # initialize the flask app
 app = Flask(__name__)
+# CORS(app)
 
-#test route for api 
-@app.route('/time')
-def get_current_time():
-  return {'time': time.time()}
 
 # route 1: county dashboard
-@app.route("/model")
+# @app.route('/model')
 
-def county():
+@app.route('/model', methods= ['POST'])
+def input_values():
+      print(request.json)
+      values = request.json
+      if values:
+          income = values["incomeRangeVal"]
+          temp = values["climateRangeVal"]
+          pop = values["popRangeVal"]
+          print(income,temp,pop, "values")
+          result = county(income, temp, pop)
+          print(result, "results printed")
+      return jsonify(result)
+          
+
+# {'incomeRangeVal': '563042', 'climateRangeVal': '51', 'popRangeVal': '606995'}
+
+def county(income, temp, pop):
+    print('county is running')
     np.random.seed(123)
     
     data = pd.read_csv('./data/joined_data_2.csv').drop(columns=['Unnamed: 0'])
     cols = data.columns[1:]
-    print('line18')
-
     scaler = StandardScaler()
-    print('line21')
     X_scaled = scaler.fit_transform(data[cols])
-    print('line23')
     
     kmeans = KMeans(n_clusters=200)
-    print('line26')
     kmeans.fit(X_scaled)
-    print('line28')
 
     data['label'] = kmeans.labels_
-    print('line31')
     y = data.label
-    print('line33')
 
     knn_model = pickle.load(open('./model/knn_updated2.pkl', 'rb'))
-    print('line36')
+    print('pickle load')
 #     knn_model.fit(X_scaled, y)
     
-    pref={'median_income': [500_000], 
-        'temp': [70], 
-        'total_population': [70_000],
-        'elev_in_ft': [80]}
+    pref={'median_income': [income], 
+        'temp': [temp], 
+        'total_population': [pop],
+        'elevation': [80]
+        }
     print('line43')
     
     prefdf = pd.DataFrame.from_dict(pref, orient='index').T
-    print('line46')
+    print(prefdf)
     pref_scaled = scaler.transform(prefdf)
     print('line48')
     pred = knn_model.predict(pref_scaled)[0]
@@ -62,7 +69,8 @@ def county():
     print(user_show)
     #return(user_show)
     return {'model_results' : user_show.to_dict()}
-    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
